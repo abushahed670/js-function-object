2c53fc985f3b95d31a31123bb68cd12fab2f65df
abushahed670 <abu.shahed670@gmail.com>
2023-12-02 23:26:21 +0600

    parameter added

--- parameter.js
+++ parameter.js
@@ -2,4 +2,18 @@ function bringSingra(money){
     console.log('mama singara den');
     console.log(money);
 }
-bringSingra(100);
+// bringSingra(100);
+
+// another
+function add(num1, num2){
+    console.log('going to add:', num1, num2);
+}
+// add(120, 125);
+
+// another exm
+function sum(a, b, c, d, e){
+    console.log(a,b,c,d,e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(12, 13, 23, 11, 14);
